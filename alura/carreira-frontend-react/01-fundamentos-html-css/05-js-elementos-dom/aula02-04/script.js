63483e0ddcd597c82de132e36442e1d2a7ecb4ed
Gabriel Gomes <gabrielgopes@gmail.com>
2026-08-14 19:32:16 -0300

    Finaliza curso js-elementos-dom

--- alura/carreira-frontend-react/01-fundamentos-html-css/05-js-elementos-dom/aula02-04/script.js
+++ alura/carreira-frontend-react/01-fundamentos-html-css/05-js-elementos-dom/aula02-04/script.js
@@ -1,55 +1,66 @@
 const html = document.querySelector("html");
 const titulo = document.querySelector(".app__title");
 const banner = document.querySelector(".app__image");
+
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const botoes = document.querySelectorAll(".app__card-button");
-const musicaFocoInput = document.getElementById("alternar-musica")
-const musica = new Audio('./sons/luna-rise-part-one.mp3');
+
+const musicaFocoInput = document.getElementById("alternar-musica");
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
 musica.loop = true;
+
+const tempoNaTela = document.getElementById("timer");
+
+const iniciarOuPausarIcon = document.querySelector(
+  ".app__card-primary-button-icon",
+);
+const iniciarOuPausarBt = document.querySelector("#start-pause span");
 const startPauseBt = document.getElementById("start-pause");
-const audioTempoAcabou = new Audio('./sons/beep.mp3');
-const audioIniciar = new Audio('./sons/play.wav');
-const audioPausar = new Audio('./pause.mp3');
 
+const audioIniciar = new Audio("./sons/play.wav");
+const audioPausar = new Audio("./sons/pause.mp3");
+const audioTempoAcabou = new Audio("./sons/beep.mp3");
 
-let tempoDecorridoEmSegundos = 5;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musicaFocoInput.addEventListener("change", () => {
   if (musica.paused) {
-    musica.play()
+    musica.play();
   } else {
-    musica.pause()
+    musica.pause();
   }
-})
+});
 
 focoBt.addEventListener("click", () => {
+  tempoDecorridoEmSegundos = 1500;
   alterarContexto("foco");
-  focoBt.classList.add('active');
+  focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
+  tempoDecorridoEmSegundos = 300;
   alterarContexto("descanso-curto");
-  curtoBt.classList.add('active');
+  curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
+  tempoDecorridoEmSegundos = 900;
   alterarContexto("descanso-longo");
-  longoBt.classList.add('active');
+  longoBt.classList.add("active");
 });
 
-// Podemos deixar a declaração da função aqui no final,
-// pois ela será executada somente se o usuario clicar
 function alterarContexto(contexto) {
-  botoes.forEach(function(contexto) {
-    contexto.classList.remove('active');
-  })
+  mostrarTempo();
 
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `./imagens/${contexto}.png`);
-  
+
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
 
   switch (contexto) {
     case "foco":
@@ -73,28 +84,45 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoAcabou.play();
     zerar();
-    audioTempoAcabou.play()
-    return
+    return;
   }
-  tempoDecorridoEmSegundos -= 1;
-  console.log('Temporizador: ' + tempoDecorridoEmSegundos);
-}
 
-startPauseBt.addEventListener('click', iniciarOuPausar);
+  tempoDecorridoEmSegundos -= 1;
+  mostrarTempo();
+};
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
 
 function iniciarOuPausar() {
-  
-  if (intervaloId) {  
+  if (intervaloId) {
     audioPausar.play();
     zerar();
-    return
+    return;
   }
+
   audioIniciar.play();
   intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.textContent = "Pausar";
+  iniciarOuPausarIcon.setAttribute("src", "./imagens/pause.png");
 }
 
 function zerar() {
   clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = "Começar";
+  iniciarOuPausarIcon.setAttribute("src", "./imagens/play_arrow.png");
   intervaloId = null;
 }
+
+function mostrarTempo() {
+  const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+  const tempoFormatado = tempo.toLocaleTimeString("pt-Br", {
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
