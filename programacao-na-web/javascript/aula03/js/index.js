9daf59006e999a8b5b9e60a9b904330af4fc30f3
Gabriel Gomes <gabrielgopes@gmail.com>
2026-08-27 20:22:46 -0300

    Finaliza terceira aula de javascript
    Cria diferentes formas de decalarações.

---
+++ programacao-na-web/javascript/aula03/js/index.js
@@ -0,0 +1,17 @@
+document
+    .getElementById("texto").innerHTML = 
+        'Meu primeiro texto <b>JS</b>!';
+
+alert('Aqui é um texto no alert().')
+
+console.log('Aqui o texto sai no console.log')
+
+
+let a, b, c; a=5; b=7; c=9;
+
+let pessoa = "Dimitri";
+//let pessoa="Dimitri";
+
+function minhaFuncao() {
+    document.write(5 + 5);
+}
