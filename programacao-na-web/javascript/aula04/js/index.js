9eeef5e400b54cae453b527eed31af2d647dfe65
Gabriel Gomes <gabrielgopes@gmail.com>
2026-09-02 19:26:19 -0300

    Finaliza aulas 04 e 05 de js

---
+++ programacao-na-web/javascript/aula04/js/index.js
@@ -0,0 +1,26 @@
+//SINTAXE JAVASCRIPT
+
+console.log('ISSO É UM TEXTO');
+console.log(1000);
+
+//DECLARAÇÃO DE VARIAVEL
+var a = 1;
+var b = 5;
+var c = a + b;
+var c = a - b;
+var c = a * b;
+var c = a / b;
+
+console.log(c);
+
+//CASE SENSITIVE
+var PESSOA = 'Gomes';
+var pessoa = 'Lopes';
+
+console.log(PESSOA)
+console.log(pessoa)
+console.log(Pessoa)
+
+//var primeiro-nome;
+var primeiroNome;
+var primeiro_nome;
---
+++ programacao-na-web/javascript/aula05/js/index.js
@@ -0,0 +1,10 @@
+//  Comentário de uma linha
+
+/*
+
+    Comententário de múltiplas linhas
+
+*/
+
+
+document.getElementById('texto').innerHTML = "Meu primeiro texto <b>JS</b>!";
